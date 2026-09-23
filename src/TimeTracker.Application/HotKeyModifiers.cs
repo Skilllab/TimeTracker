462b2a68fea256b9575750ba07ec8cf0fe99768a
Oleg Bezyaev <redslide@yandex.ru>
2026-09-23 16:54:30 +0300

    Вдогонку

--- src/TimeTracker.Application/HotKeyModifiers.cs
+++ src/TimeTracker.Application/HotKeyModifiers.cs
@@ -1,9 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace TimeTracker.Application;
 
-internal class Class1
+/// <summary>
+/// Модификаторы сочетания клавиш.
+/// </summary>
+[Flags]
+public enum HotKeyModifiers
 {
+    /// <summary>Без модификаторов.</summary>
+    None = 0,
+
+    /// <summary>Клавиша Alt.</summary>
+    Alt = 1,
+
+    /// <summary>Клавиша Control.</summary>
+    Control = 2,
+
+    /// <summary>Клавиша Shift.</summary>
+    Shift = 4,
+
+    /// <summary>Клавиша Windows или Command.</summary>
+    Meta = 8
 }
